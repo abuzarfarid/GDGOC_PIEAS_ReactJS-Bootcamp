d740567b691720e1663c466462f84b9858c3b296
Abu Zar Farid <131692156+abuzarfarid@users.noreply.github.com>
2024-11-28 21:09:04 +0500

    Add files via upload

---
+++ Assignment 02/src/data/books.js
@@ -0,0 +1,7 @@
+export const Books = [
+  { id: 1, title: "Book One", author: "Author A", rating: 4.8, genre: "fiction" },
+  { id: 2, title: "Book Two", author: "Author B", rating: 3.9, genre: "non-fiction" },
+  { id: 3, title: "Book Three", author: "Author C", rating: 4.7, genre: "fiction" },
+  { id: 4, title: "Book Four", author: "Author D", rating: 4.1, genre: "history" },
+  { id: 5, title: "Book Five", author: "Author E", rating: 4.6, genre: "fiction" },
+];
